42163570d02282e68185f5829da5142986ce3292
semizorova <83871472+semizorova@users.noreply.github.com>
2021-05-22 17:12:49 +0300

    Add files via upload
    V1.1

--- ping.py
+++ ping.py
@@ -17,6 +17,7 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 420:
             self.rect.y += self.speed
     def update_l(self):
+        keys= key.get_pressed()
         if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < 420:
@@ -36,9 +37,14 @@ speed_y = 3
 clock = time.Clock()
 FPS = 60
 
+font.init()
+font1 = font.SysFont(None, 70)
+lose_1 = font1.render("YOU 1 LOSE!!!",1,(255,0,0))
+lose_2 = font1.render("YOU 2 LOSE!!!",1,(255,0,0))
+
 pin = GameSprite("ball.png",200,200,4,50,50)
-rack_1 = Player("Racket.png",30,200,4,50,150)
-rack_2 = Player("Racket.png",520,200,4,50,50)
+rack_1 = Player("fon.jpg",30,200,4,30,150)
+rack_2 = Player("fon.jpg",520,200,4,30,150)
 
 while game:
     for e in event.get():
@@ -50,13 +56,19 @@ while game:
         rack_1.update_l()
         rack_2.update_r()
         
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
+        pin.rect.x += speed_x
+        pin.rect.y += speed_y
 
         pin.reset()
         rack_1.reset()
         rack_2.reset()
-    if ball.reck.y > 450 or ball.reck.y < 0:
-        speed_y *= -1
+        if sprite.collide_rect(rack_1, pin) or sprite.collide_rect(rack_2, pin):
+            speed_x *= -1
+        if pin.rect.y > 450 or pin.rect.y < 0:
+            speed_y *= -1
+        if pin.rect.x > 600:
+            window.blit(lose_2,(180,20))
+        if pin.rect.x < 0:
+            window.blit(lose_1,(180,200))
     display.update()
     clock.tick(FPS)
